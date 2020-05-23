e93d839722440c1f69c36e5acb900d3ff3686fad
dhenyashari <dheny3007@gmail.com>
2020-05-24 03:23:03 +0800

    memperbaharui urls.py, view.py merapikan struktur folder

--- DjangoWeather/urls.py
+++ DjangoWeather/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
-from weather import views
+from django.urls import path, re_path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^$',views.home, name='home'),
+    re_path(r'^$',include('weather.urls')),
 ]
