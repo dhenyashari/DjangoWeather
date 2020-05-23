7a7d39733e611cb5b45f7705dec568b10762f878
dhenyashari <dheny3007@gmail.com>
2020-05-24 05:10:08 +0800

    aplikasi sudah jalan sesuai data inputan di coding

--- weather/urls.py
+++ weather/urls.py
@@ -3,4 +3,4 @@ from . import views
 
 urlpatterns = [
     path(r'',views.index),
-]
+    ]
--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,19 @@
+#from django.http import HttpResponse
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def index(request):
-  return render(request, 'weather/weather.html')
+  url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=fd3347fcd07981743cf53ade32a327cd'
+  city = 'Provinsi Bali, ID'
+  r = requests.get(url.format(city)).json()
+
+  city_weather = {
+    'city' : city,
+    'temperature': r['main']['temp'],
+    'description': r['weather'][0]['description'],
+    'icon': r['weather'][0]['icon'],
+  }
+  print(city_weather)
+  context = {'city_weather': city_weather}
+  return render(request, 'weather/weather.html', context)
