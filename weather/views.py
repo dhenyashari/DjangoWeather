541600dd0f71f7125306d550ebb886e0643a3219
dhenyashari <dheny3007@gmail.com>
2020-06-09 00:12:05 +0800

    menambahkan database mysql untuk nama provinsi hingga desa berdasarkan permendagri no 72 tahun 2019 tanpa jumlah penduduk

--- weather/views.py
+++ weather/views.py
@@ -1,11 +1,11 @@
-#from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
 import requests
 
 # Create your views here.
 def index(request):
   url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=fd3347fcd07981743cf53ade32a327cd'
-  city = 'Provinsi Bali, ID'
+  city = 'Bali, ID'
   r = requests.get(url.format(city)).json()
 
   city_weather = {
@@ -17,3 +17,5 @@ def index(request):
   print(city_weather)
   context = {'city_weather': city_weather}
   return render(request, 'weather/weather.html', context)
+  # print (r)
+  # return HttpResponse(r)
